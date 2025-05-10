9ff0cbf587b7e2b44d7c7f7dfe8ba21235929112
ilomiaylo <ilayda26565jo@gmail.com>
2025-05-10 00:28:30 -0500

    Add files via upload

---
+++ testScores.js
@@ -0,0 +1,27 @@
+/* Ilayda orhan */
+<html lang="en"> 
+<head>
+    <title> My JS template page </title>
+    <meta charset="UTF-8">
+    </head>
+    <body>
+        <script>
+            
+let age = parseInt(Prompt("Enter your age: "));
+
+if (age >= 18) {
+    console.log("You are eligible to vote! ");
+} else { 
+    let yearsToVote = 18 - age;
+    console.log("You will be eligible to vote in" + yearsToVote +  "years."); 
+}
+</script>
+</body>
+
+
+
+
+
+
+
+</html>
